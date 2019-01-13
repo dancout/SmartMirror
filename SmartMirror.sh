849e1450f26c39e308afd238e6efcb0d5f63cc3d
Daniel Couturier <dancout@umich.edu>
2019-01-13 16:35:57 -0500

    Update SmartMirror.sh
    Updated SmartMirror.sh file so that it can pull MichMich's MagicMirror project entirely, then within that project pull DanCout's SmartMirror project. It then replaces the config.js file with DanCout's version, and launches pm2 to display the new webpage.
    
    Will be eventually called with a similar bash command MichMich uses to pull everything.

--- SmartMirror.sh
+++ SmartMirror.sh
@@ -1,7 +1,32 @@
-echo "Copying DanCout's config.js file to overwrite MichMich's config file";
-echo "file is stored in MagicMirror/SmartMirror/config.js";
-echo "copy command: cp config.js ../config/config.js"
+echo "Starting everything"
 
-cp config.js ../config/config.js
+yes | bash -c "$(curl -sL https://raw.githubusercontent.com/MichMich/MagicMirror/master/installers/raspberry.sh)"
 
-echo "Copying complete";
+echo "Everything Started!";
+
+pm2 stop all
+
+echo "stopped pm2. Ran: pm2 stop all";
+
+echo "cd into MagicMirror proj";
+cd ~/MagicMirror;
+
+echo "Now clone DanCout's SmartMirror project into this file structure";
+git clone -b Develop https://github.com/dancout/SmartMirror.git;
+echo "DanCout's SmartMirror cloned!"
+
+echo "Now move over the config.js file";
+cd ~/MagicMirror/SmartMirror
+
+bash SmartMirror.sh;
+
+cd ~
+echo "now starting the pm2 server, with my webpage";
+
+pm2 start all;
+
+# echo "moving config";
+
+# cp config.js ../config/config.js
+
+# echo "move complete";
